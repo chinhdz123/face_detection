220c12ae48fe9b990ec786f63c62cc0a7591a560
Chinhle <chinhdzzz14@gmail.com>
2023-12-08 07:59:45 +0700

    update

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+// // Mảng chứa thông tin về các ảnh và tên
+// const clientData = [
+//     { imagePath: 'images/bui_the_anh.png', name1: 'Bùi Thế Anh', name2: 'Bùi Thị Thu Trang', name3:'Bùi Thị Thùy Trang' },
+//     { imagePath: 'images/bui_thi_thu_trang.png', name1: 'Bùi Thế 1', name2: 'Bùi Thị Thu 1', name3:'Bùi Thị Thùy 1' },
+//     // Thêm các phần tử khác nếu cần
+// ];
+
+// clientData.forEach(data => {
+//     // Tạo thẻ div mới cho mỗi ảnh và tên
+//     const clientInfoDiv = document.createElement('div');
+//     clientInfoDiv.id = 'client_info';
+
+//     const img = document.createElement('img');
+//     img.src = data.imagePath;
+//     img.alt = '';
+
+// })
